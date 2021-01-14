2e93629c598b21c6d947922ecc9e48ddb812ea17
jinhyun <jinhyun0504@hanmail.net>
2021-01-14 14:41:11 +0900

    initial commit

---
+++ src/main/java/com/skcc/narubatch/NarubatchApplication.java
@@ -0,0 +1,13 @@
+package com.skcc.narubatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarubatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarubatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/narubatch/NarubatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.narubatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarubatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
